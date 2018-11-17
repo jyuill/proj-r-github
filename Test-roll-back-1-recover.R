31bab0a8b375f2a39833e343c344650f66d9f9f6
jyuill <john@fig4.com>
2018-11-17 15:34:21 -0800

    recovered old version, saved as new file name

---
+++ Test-roll-back-1-recover.R
@@ -0,0 +1,13 @@
+## Testing roll-back via Git
+
+library(tidyverse)
+
+data <- data.frame(name=c("Alex","Ed","George"),
+                   age=c(22,44,33))
+
+write_csv(data, 'input/name-age.csv')
+
+ggplot(data, aes(x=name, y=age))+geom_bar(stat='identity')
+
+ggplot(data, aes(x=age))+geom_histogram(dinwidth=20)
+
