d5e80ce8b07ad060defd4a6c43ae56ba708349f4
jyuill <john@fig4.com>
2018-11-17 15:45:22 -0800

    added more data

--- Test-roll-back-1.R
+++ Test-roll-back-1.R
@@ -2,12 +2,12 @@
 
 library(tidyverse)
 
-data <- data.frame(name=c("Alex","Ed","George"),
-                   age=c(22,44,33))
+data <- data.frame(name=c("Alex","Ed","George","Helen","Sue","Tene"),
+                   age=c(22,44,33,38,29,12))
 
 write_csv(data, 'input/name-age.csv')
 
 ggplot(data, aes(x=name, y=age))+geom_bar(stat='identity')
 
-ggplot(data, aes(x=age))+geom_histogram(dinwidth=20)
+ggplot(data, aes(x=age))+geom_histogram(binwidth=10)
 
