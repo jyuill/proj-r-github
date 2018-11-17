8c8a41c766df8b56b2a8a1e3cf92156d73ded2bb
jyuill <john@fig4.com>
2018-11-17 15:24:49 -0800

    update data, add age diff calc

---
+++ Test-delete-1.R
@@ -0,0 +1,10 @@
+## File for delete to test recove
+
+name.age <- read_csv('input/name-age.csv')
+
+name.age$name <- as.factor(name.age$name)
+name.age <- name.age %>% mutate(
+  adiff=lag(age)-age
+)
+
+ggplot(name.age, aes(x=name, y=adiff))+geom_bar(stat='identity')
--- Test-roll-back-1.R
+++ Test-roll-back-1.R
@@ -2,7 +2,10 @@
 
 library(tidyverse)
 
-data <- data.frame(name=c("George","Ed"),
-                   age=c(44,33))
+data <- data.frame(name=c("Alex","Ed","George"),
+                   age=c(22,44,33))
+
+write_csv(data, 'input/name-age.csv')
 
 ggplot(data, aes(x=name, y=age))+geom_bar(stat='identity')
+
